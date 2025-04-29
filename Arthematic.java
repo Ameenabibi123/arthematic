2e9c6368befd753a4a215bc26128ad5b0417b3ed
Ameenabibi123 <ameenamahammadanavar8@gmail.com>
2025-04-29 10:03:25 +0530

    changese

--- Arthematic.java
+++ Arthematic.java
@@ -11,12 +11,21 @@ public class Arthematic {
         int quotient = num1 / num2;
         int remainder = num1 % num2;
 
+        // Perform additional calculations: square root and cube root
+        double sqrtNum1 = Math.sqrt(num1); // Square root of num1
+        double sqrtNum2 = Math.sqrt(num2); // Square root of num2
+        double cubeRootNum1 = Math.cbrt(num1); // Cube root of num1
+        double cubeRootNum2 = Math.cbrt(num2); // Cube root of num2
+
         // Display the results
         System.out.println("Addition: " + sum);
         System.out.println("Subtraction: " + difference);
         System.out.println("Multiplication: " + product);
         System.out.println("Division: " + quotient);
         System.out.println("Remainder: " + remainder);
+        System.out.println("Square Root of " + num1 + ": " + sqrtNum1);
+        System.out.println("Square Root of " + num2 + ": " + sqrtNum2);
+        System.out.println("Cube Root of " + num1 + ": " + cubeRootNum1);
+        System.out.println("Cube Root of " + num2 + ": " + cubeRootNum2);
     }
-
 }
